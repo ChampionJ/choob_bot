baf087dd8450b9f18d3fe851242a4524fe9828fd
John Champion <john@championprogramming.com>
2021-08-11 12:31:07 -0400

    Update node packages, bump version number

--- src/structures/databaseTypes/schemas/ChoobMessage.ts
+++ src/structures/databaseTypes/schemas/ChoobMessage.ts
@@ -1,6 +1,12 @@
-import { getModelForClass, index, modelOptions, mongoose, prop, queryMethod, ReturnModelType } from "@typegoose/typegoose";
-import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
-import { QueryMethod } from "@typegoose/typegoose/lib/types";
+import {
+  getModelForClass,
+  index,
+  modelOptions,
+  mongoose,
+  prop,
+  queryMethod,
+  ReturnModelType,
+} from "@typegoose/typegoose";
 import { IChoobQuote } from "../interfaces/IChoobMessage";
 
 // @modelOptions({
@@ -10,7 +16,9 @@ import { IChoobQuote } from "../interfaces/IChoobMessage";
 //   }
 // })
 
-@modelOptions({ schemaOptions: { collection: 'choob_quotes', timestamps: true } })
+@modelOptions({
+  schemaOptions: { collection: "choob_quotes", timestamps: true },
+})
 export class ChoobQuote implements IChoobQuote {
   _id!: mongoose.Types.ObjectId;
   public createdAt!: Date;
@@ -21,6 +29,5 @@ export class ChoobQuote implements IChoobQuote {
   public author!: string;
   @prop({ required: true })
   public authorId!: string;
-
 }
-export const ChoobQuoteModel = getModelForClass(ChoobQuote)
+export const ChoobQuoteModel = getModelForClass(ChoobQuote);
